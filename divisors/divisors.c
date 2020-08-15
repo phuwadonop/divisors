d8a41ab88b7b79990fd7565144bc3ec34f5417b7
phu wadon <pmmp_1599@hotmail.com>
2020-08-15 14:24:30 +0700

    .

--- divisors/divisors.c
+++ divisors/divisors.c
@@ -1,32 +1,17 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 int main() {
-    int n;
-    int divisors = 0;
-    int trinumber = 0;
-    short checkpoint = 0;
-    scanf("%d", &n);
-    if (n == 1) trinumber += 1;
-    else {
-        for (int i = 1; divisors != n ; i++) {
-            trinumber += i;
-            int div = 1;
-            while (1)
-            {
-                int x = trinumber;
-                if (x % div != 0) {
-                    div += 1;
-                }
-                else {
-                    x = x / div;
-                    divisors += 1;
-                    if (divisors == n) { checkpoint = 1; break; }
-                    if (x == 1) break;
-                    div += 1;
-                }
-            }
-            if (checkpoint == 1) break;
+    int div = 2, dvs = 1;
+    int x, y;
+    scanf("%d", &x);
+    while (1) {
+        if (x % div != 0) div += 1;
+        else {
+            y = x;
+            y /= div;
+            dvs += 1;
+            if (x == 1) break;
+            div += 1;
         }
     }
-    printf("%d", trinumber);
+    printf("%d", dvs);
 }
