effa854239944c3bedf4f6c315f3fa0a87c9574a
phu wadon <pmmp_1599@hotmail.com>
2020-08-15 14:32:46 +0700

    find dvs

--- divisors/divisors.c
+++ divisors/divisors.c
@@ -1,17 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+int findDvs();
 int main() {
+    int n, dvs = 0, trinum = 1;
+    scanf("%d", &n);
+    if (n == 1) printf("%d", trinum);
+    else {
+        for (int i = 2; ;i++) {
+            trinum += i;
+            if (findDvs(trinum) > n) break;
+        }
+        printf("%d", trinum);
+    }
+}
+int findDvs(int x) {
     int div = 2, dvs = 1;
-    int x, y;
-    scanf("%d", &x);
+    int y;
     while (1) {
         if (x % div != 0) div += 1;
         else {
-            y = x;
-            y /= div;
             dvs += 1;
-            if (x == 1) break;
+            y = x / div;
+            if (y == 1) break;
             div += 1;
         }
     }
-    printf("%d", dvs);
+    return dvs;
 }
